4a33e06130ae9de32c4e94de14096eb10e32aefa
Clément Jo <Clment Jo>
2017-01-09 14:52:07 +0100

    class color

--- jee-bootstrap-core/src/main/java/jeudame/JeuDameMain.java
+++ jee-bootstrap-core/src/main/java/jeudame/JeuDameMain.java
@@ -3,13 +3,9 @@ package jeudame;
 public class JeuDameMain {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("coucou");
-=======
-		// TODO Auto-generated method stub
-		System.out.print("Coucou");
 
->>>>>>> branch 'master' of https://github.com/william-jauffret/jee-bootstrap.git
+		System.out.println("coucou");
+
 	}
 
 }
