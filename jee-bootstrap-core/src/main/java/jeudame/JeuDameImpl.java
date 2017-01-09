8595ab604c85444a897cb33e3495702b77b4843c
William Jauffret <william.jauffret@isen-lille.fr>
2017-01-09 15:28:04 +0100

    ahahahahha

--- jee-bootstrap-core/src/main/java/jeudame/JeuDameImpl.java
+++ jee-bootstrap-core/src/main/java/jeudame/JeuDameImpl.java
@@ -1,5 +1,35 @@
 package jeudame;
 
-public class JeuDameImpl {
+public class JeuDameImpl implements JeuDameGame {
+
+	@Override
+	public void play(ColorChip colour, int abs, int ord) throws GameException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ColorChip getCell(int abs, int ord) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getAbs() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getOrd() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ColorChip getWinner() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
